e2cd2d7cbe85109d903ef1a3fc87542efbb758a7
esnef <esnef@hotmaik.com>
2015-10-30 15:48:52 +0100

    30-10-2015

--- .metadata/.plugins/org.eclipse.thym.core/cordovaJsLib/example7/cordova_plugins.js
+++ .metadata/.plugins/org.eclipse.thym.core/cordovaJsLib/example7/cordova_plugins.js
@@ -1,3 +1,3 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = []
+module.exports = [{"file":"plugins/cordova-plugin-device/www/device.js","id":"cordova-plugin-device.device","clobbers":["device"]},{"file":"plugins/cordova-plugin-device/src/firefoxos/DeviceProxy.js","id":"cordova-plugin-device.DeviceProxy","runs":true},{"file":"plugins/cordova-plugin-device/src/tizen/DeviceProxy.js","id":"cordova-plugin-device.DeviceProxy","runs":true},{"file":"plugins/cordova-plugin-device/src/ubuntu/device.js","id":"cordova-plugin-device.DeviceProxy","merges":["device"]},{"file":"plugins/cordova-plugin-device/src/windows/DeviceProxy.js","id":"cordova-plugin-device.DeviceProxy","merges":[""]},{"file":"plugins/cordova-plugin-device/src/browser/DeviceProxy.js","id":"cordova-plugin-device.DeviceProxy","runs":true}]
 });
